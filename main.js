f08d5faaeb5e6ada377465bd94d69283a3c05b2d
MelanieL <melanieleeking@gmail.com>
2019-02-24 20:51:14 -0500

    Completed ex 08 - Components.

--- main.js
+++ main.js
@@ -1,60 +1,106 @@
-// new Vue creates a new view instance, which is the root of a Vue.js app
-var app = new Vue({
-    // Pass an options object into it
-    // Relationship between instance and DOM, use property el
-    el: '#app',
-    // Can put data into it
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        // description: 'They go on your feet.',
-        // Removed image: to replace with selectedVariant
-        // image: './assets/vmSocks-green.jpg',
-        selectedVariant: 0,
-        // link: 'https://en.wikipedia.org/wiki/Socks_(disambiguation)',
-        // Removed instock to turn it into a computed property so it pulls from the variants for quantities instead
-        // inStock: false,
-        // if above is false, styles will activate to gray out add to cart button
-        // inventory: 0,
-        // onSale: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage:"./assets/vmSocks-green.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue.jpg",
-                variantQuantity: 0                
-            }
-        ],
-        sizes: ["S", "M", "L", "XL"],
-        cart: 0,
-        onSale: true
-        },
-    methods: {
-            // Can also write functions in ES6 shorthand, ie addToCart() {} instead
-            addToCart: function () {
-                this.cart += 1
-            },
-            // Using ES6 shorthand for the challenge ex
-            // Also added the if statement to prevent the cart from going below zero
-            removeFromCart() {
-                if(this.cart > 0){
-                this.cart -= 1
+// Creating a view components, which is basically all the pieces that can be reused. You can have components within components.
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+            <div class="product">
+
+                <div class="product-image">
+                    <img :src="image" />
+                </div>
+            
+                <div class="product-info">
+                    <h1>{{ title }}</h1>
+                    <p v-if="inStock">In Stock</p>
+                    <p v-else>Out of Stock</p>
+                    <p>Shipping: {{ shipping }}</p>
+
+                    <ul>
+                        <li v-for="detail in details">{{ detail }}</li>
+                    </ul>
+
+                    <div v-for="(variant, index) in variants"
+                        :key="variant.variantId"
+                        class="color-box" 
+                        :style="{ backgroundColor: variant.variantColor }"
+                        @mouseover="updateProduct(index)">
+                        </p>
+                    </div>
+
+                    <ul>
+                        <li v-for="size in sizes">{{ size }}</li>
+                    </ul>
+                    
+                    <button v-on:click="addToCart" 
+                        :disabled="!inStock"
+                        :class="{ disabledButton: !inStock }">Add to Cart</button>
+                    <button v-on:click="removeFromCart">Remove from Cart</button>
+
+                    <div class="cart">
+                        <p>Cart({{cart}})</p>
+                    </div>
+
+                </div>
+            
+            </div>
+    `,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            // description: 'They go on your feet.',
+            // Removed image: to replace with selectedVariant
+            // image: './assets/vmSocks-green.jpg',
+            selectedVariant: 0,
+            // link: 'https://en.wikipedia.org/wiki/Socks_(disambiguation)',
+            // Removed instock to turn it into a computed property so it pulls from the variants for quantities instead
+            // inStock: false,
+            // if above is false, styles will activate to gray out add to cart button
+            // inventory: 0,
+            // onSale: true,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: "./assets/vmSocks-green.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: "./assets/vmSocks-blue.jpg",
+                    variantQuantity: 0
                 }
-            },
-            // updateProduct: function (variantImage) {
-            updateProduct(index) {
-                // this.image = variantImage
-                this.selectedVariant = index
-                console.log(index)
+            ],
+            sizes: ["S", "M", "L", "XL"],
+            cart: 0,
+            onSale: true
+        }
+    },
+    methods: {
+        // Can also write functions in ES6 shorthand, ie addToCart() {} instead
+        addToCart: function () {
+            this.cart += 1
+        },
+        // Using ES6 shorthand for the challenge ex
+        // Also added the if statement to prevent the cart from going below zero
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
             }
         },
+        // updateProduct: function (variantImage) {
+        updateProduct(index) {
+            // this.image = variantImage
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
     // Computed properties are cached (results are saved until they are changed). These are calculations.
     computed: {
         title() {
@@ -73,9 +119,23 @@ var app = new Vue({
             else {
                 return this.brand + ' ' + this.product + ' Not on Sale.'
             }
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 2.99
         }
-
     }
-    
+})
+
+// new Vue creates a new view instance, which is the root of a Vue.js app
+var app = new Vue({
+    // Pass an options object into it
+    // Relationship between instance and DOM, use property el
+    el: '#app',
+    data: {
+        premium: false
+    }
 })
 
